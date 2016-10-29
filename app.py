fa248bfb10a3c497346b29b44b2331b9dcf87dd9
Ted Xiao <t.xiao@berkeley.edu>
2016-10-28 17:04:23 -0700

    Adding Postgres DB locally

--- app.py
+++ app.py
@@ -4,11 +4,52 @@ import json
 
 import requests
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/all-users'
+db = SQLAlchemy(app)
+
+# Create our database model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
+
+
 
 @app.route('/', methods=['GET'])
 def index():
+    return render_template('index.html')
+
+@app.route('/users', methods=['GET'])
+def users_index():
+    return render_template(
+       'users.html',
+       users=User.query.all())
+
+@app.route('/signup', methods=['GET'])
+def signup_index():
+    return render_template('signup_index.html')
+
+# Save e-mail to database and send to success page
+@app.route('/prereg', methods=['POST'])
+def prereg():
+    email = None
+    if request.method == 'POST':
+        email = request.form['email']
+        # Check that email does not already exist (not a great query, but works)
+        if not db.session.query(User).filter(User.email == email).count():
+            reg = User(email)
+            db.session.add(reg)
+            db.session.commit()
+            return render_template('success.html')
     return render_template('index.html')
 
 @app.route('/facebook/webhook/', methods=['GET'])
